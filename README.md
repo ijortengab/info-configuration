b230e3154f147cf657352b904c0a9504e3614f92
Ijor Tengab <m_roji28@yahoo.com>
2015-12-13 15:59:40 +0700

    rename from info-configuration to parse-info

--- README.md
+++ README.md
@@ -2,3 +2,63 @@ Info Configuration
 ==================
 
 Dumper and parser for dot info file.
+
+   > "The file format is "INI-like" for ease of authoring, but also includes 
+   > some "Drupalisms" such as the array[] syntax so standard PHP functions 
+   > for reading/writing INI files can't be used." [Source][blog]
+   
+File dot ini (.ini) adalah file untuk menyimpan konfigurasi buatan Microsoft 
+dan digunakan oleh PHP untuk menyimpan konfigurasi global (php.ini). 
+Kelemahan file dot ini adalah tidak bisa menyimpan konfigurasi secara array 
+multidimensi (terbatas hanya 2 level kedalaman array). 
+
+Parse Info adalah library PHP untuk serialize dan unserialize variable bertype 
+array menjadi content file dot info.
+
+Contoh file dot ini:
+```ini
+; global
+key1 = value
+key2 = value
+[parent1]
+; inside array parent1
+key1 = value
+key2 = value
+[parent2]
+; inside array parent2
+key1 = value
+key2 = value
+```
+
+Contoh file dot info:
+```ini
+; global
+key1 = value
+key2 = value
+; inside array parent1
+parent1[key1] = value
+parent1[key2] = value
+parent1[key3][child] = value
+; inside array parent1
+parent2[key1] = value
+parent2[key2] = value
+parent2[key3][child] = value
+```
+
+## Usage
+
+```php
+// Mengubah array menjadi isi file dot info, 
+// seperti serialize() dan json_encode().
+$content = ParseInfo::encode($array);
+// Mengubah isi file dot info menjadi array,
+// seperti unserialize() dan json_decode().
+$array = ParseInfo::decode($content);
+```
+
+## Reference
+
+ *   [Writing module .info files](https://www.drupal.org/node/542202)
+ *   [Acquia Blog][blog]
+
+[blog]: https://www.acquia.com/blog/ultimate-guide-drupal-8-episode-7-code-changes-drupal-8
--- src/ParseInfo.php
+++ src/ParseInfo.php
@@ -1,51 +1,21 @@
 <?php
-/**
- * @file
- *   InfoConfiguration.php
- *
- * @author
- *   IjorTengab
- *
- * @homepage
- *   https://drupal.org
- *   https://www.drupal.org/node/542202
- *   https://github.com/ijortengab/info-configuration
- */
 namespace IjorTengab;
 
-class InfoConfiguration
+/**
+ * Dumper and parser for dot info file.
+ */
+class ParseInfo
 {
-
-    /**
-     * Hasil dari parsing.
-     */
-    public $result;
-
-    /**
-     * Fungsi awal langsung bersifat toggle.
-     * Jika input diberikan merupakan string, artinya
-     * ubah dari string ke array
-     * sebaliknya jika input diberikan merupakan array atau object
-     * maka ubah menjadi string.
-     */
-    function __construct($input = NULL) {
-        if (is_array($input) || is_object($input)) {
-            return $this->result = $this->encode($input);
-        }
-        elseif (is_string($input)) {
-            return $this->result = $this->decode($input);
-        }
-    }
 
     /**
      * Fungsi mengubah dari array atau object menjadi string.
      */
-    public function encode($array_or_object) 
+    public static function encode($array_or_object)
     {
         if(is_array($array_or_object) || is_object($array_or_object)) {
             $result = array();
-            $this->_encode_recursive($result, $array_or_object);
-            $result = implode("\r\n", $result);
+            self::_encode_recursive($result, $array_or_object);
+            $result = implode(PHP_EOL, $result);
             return $result;
         }
     }
@@ -54,42 +24,43 @@ class InfoConfiguration
      * Fungsi internal, dibutuhkan oleh method encode untuk
      * melakukan proses secara recursive terkait struktur array.
      */
-    protected function _encode_recursive(&$result, $value, &$key = null)
+    protected static function _encode_recursive(&$result, $value, &$key = null)
     {
-        if(is_array($value) || is_object($value)) {
+        if (is_array($value) || is_object($value)) {
           // Jadikan array, jikapun sebuah object.
           $value = (array) $value;
-            foreach($value as $_key => $_value) {
+            foreach ($value as $_key => $_value) {
                 if(is_array($_value) || is_object($_value)) {
                     // Jadikan array, jikapun sebuah object.
-          $_value = (array) $_value;
-                    foreach($_value as $__key => $__value) {
+                    $_value = (array) $_value;
+                    foreach ($_value as $__key => $__value) {
                         $buildkey = $_key . '[' . $__key . ']';
                         if (!is_null($key)) {
                             $buildkey = $key . '[' . $_key . '][' . $__key . ']';
                         }
-                        $this->_encode_recursive($result, $__value, $buildkey);
+                        self::_encode_recursive($result, $__value, $buildkey);
                     }
                 }
-                else{
+                else {
                     $buildkey = $_key;
                     if (!is_null($key)) {
                         $buildkey = $key . '[' . $_key . ']';
                     }
-                    $result[] = $buildkey . ' = ' . $this->_encode_modify($_value);
+                    $result[] = $buildkey . ' = ' . self::_encode_modify($_value);
                 }
             }
         }
         else{
-            $result[] = $key . ' = ' . $this->_encode_modify($value);
+            $result[] = $key . ' = ' . self::_encode_modify($value);
         }
     }
-  
+
     /**
      * Fungsi internal, dibutuhkan oleh method _encode_recursive untuk
      * memodifikasi value.
      */
-    protected function _encode_modify($value) {
+    protected static function _encode_modify($value)
+    {
         if (is_string($value) && preg_match('/["\'\r\n\t]/',$value)) {
             $value = json_encode($value);
         }
@@ -106,12 +77,13 @@ class InfoConfiguration
         }
         return $value;
     }
-    
+
     /**
-     * Fungsi bersumber dari fungsi drupal 7 yakni drupal_parse_info_format($data) 
+     * Fungsi bersumber dari fungsi drupal 7 yakni drupal_parse_info_format($data)
      * dengan sedikit modifikasi.
      */
-    public function decode($data) {
+    public static function decode($data)
+    {
         $info = array();
         if (preg_match_all('
             @^\s*                           # Start at the beginning of a line, ignoring leading whitespace
@@ -133,9 +105,10 @@ class InfoConfiguration
                 foreach (array('key', 'value1', 'value2', 'value3') as $var) {
                     $$var = isset($match[++$i]) ? $match[$i] : '';
                 }
+
                 // Tambahan hack start
                 foreach (array('value1', 'value2', 'value3') as $var) {
-                    $$var = $this->_decode_modify_before($$var);
+                    $$var = self::_decode_modify_before($$var);
                     // Jika array, maka sudahi.
                     if (is_array($$var)) {
                         $parent = &$info;
@@ -167,18 +140,18 @@ class InfoConfiguration
                     $last = count($parent);
                 }
                 // Update hack start
-                $parent[$last] = $this->_decode_modify_after($value);
+                $parent[$last] = self::_decode_modify_after($value);
                 // Update hack finish
             }
         }
         return $info;
-
     }
-    
+
     /**
      * Memodifikasi value.
      */
-    protected function _decode_modify_before($value) {
+    protected static function _decode_modify_before($value)
+    {
         // Jika huruf awal dari value adalah double quote, maka pasti value tersebut
         // hasil json encode, maka perlu kita kembalikan ke format awal
         if (substr($value, 0, 1) == '"') {
@@ -195,9 +168,10 @@ class InfoConfiguration
     }
 
     /**
-     * 
+     *
      */
-    protected function _decode_modify_after($value) {
+    protected static function _decode_modify_after($value)
+    {
         if ($value === 'TRUE') {
             $value = (bool) TRUE;
         }
@@ -209,5 +183,4 @@ class InfoConfiguration
         }
         return $value;
     }
-
 }
